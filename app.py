762bd914323674724bea25e2a44a1df97b3291c7
Stella Kan <stella102.kan@gmail.com>
2026-08-12 12:25:12 +0800

    Update Flask server for Render

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import uuid
 import sqlite3
 import requests
 import json
+import os
 
 
 app = Flask(__name__)
@@ -3633,6 +3634,9 @@ def hospital():
 
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
 
 
