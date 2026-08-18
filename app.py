82ee6e22b308a3eb38398084ec8b3c65f20f6c77
Stella Kan <stella102.kan@gmail.com>
2026-08-18 18:10:05 +0800

    Initialize database tables on deployment

--- app.py
+++ app.py
@@ -4504,8 +4504,9 @@ def fix_db():
     <pre>{sequence_name}</pre>
     """
 
+init_db()
+
 if __name__ == "__main__":
-    init_db()
     app.run(
         host="0.0.0.0",
         port=int(os.environ.get("PORT", 5000))
